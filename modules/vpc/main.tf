da4aa4e2c18f60fa6f22fd3d3fadcda27643d3c8
Erdinç Akgün <erdinc.akgun@icloud.com>
2026-01-04 20:17:24 +0300

    chore: chore

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "huaweicloud_vpc" "vpc_with_tags" {
   name   = var.vpc_name
-  region = var.region
+  region = var.vpc_region
   cidr   = var.vpc_cidr
-  tags   = var.tags
+  tags   = var.vpc_tags
 }
